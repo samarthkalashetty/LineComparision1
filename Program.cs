5943f367f5b0b0a9210b68c3085378a16de0300c
samarthkalashetty <samarthkalashetti2612@gmail.com>
2023-08-12 14:21:30 +0530

    welcome to Uc3

--- Program.cs
+++ Program.cs
@@ -1,44 +1,45 @@
 ﻿using System;
 
-namespace LineEqualityCheck
+namespace LineComparison
 {
-    class UC2
+    class UC3
     {
-        class Line
+        class Line : IComparable<Line>
         {
             public double X1 { get; set; }
             public double Y1 { get; set; }
             public double X2 { get; set; }
             public double Y2 { get; set; }
 
-            public override bool Equals(object obj)
-            {
-                if (obj == null || GetType() != obj.GetType())
-                {
-                    return false;
-                }
+            public double Length => Math.Sqrt(Math.Pow(X2 - X1, 2) + Math.Pow(Y2 - Y1, 2));
 
-                Line other = (Line)obj;
-                return X1 == other.X1 && Y1 == other.Y1 && X2 == other.X2 && Y2 == other.Y2;
-            }
-
-            public override int GetHashCode()
+            public int CompareTo(Line other)
             {
-                return HashCode.Combine(X1, Y1, X2, Y2);
+                double thisLength = Length;
+                double otherLength = other.Length;
+
+                if (thisLength < otherLength)
+                    return -1;
+                else if (thisLength > otherLength)
+                    return 1;
+                else
+                    return 0;
             }
         }
 
         static void Main(string[] args)
         {
-            Line line1 = new Line { X1 = 0, Y1 = 0, X2 = 1, Y2 = 1 };
-            Line line2 = new Line { X1 = 0, Y1 = 0, X2 = 1, Y2 = 1 };
-            Line line3 = new Line { X1 = 2, Y1 = 2, X2 = 3, Y2 = 3 };
+            Line line1 = new Line { X1 = 0, Y1 = 0, X2 = 3, Y2 = 4 };
+            Line line2 = new Line { X1 = 0, Y1 = 0, X2 = 5, Y2 = 12 };
+            Line line3 = new Line { X1 = 0, Y1 = 0, X2 = 1, Y2 = 1 };
 
-            bool areLinesEqual1 = line1.Equals(line2);
-            bool areLinesEqual2 = line1.Equals(line3);
+            int comparison1to2 = line1.CompareTo(line2);
+            int comparison2to1 = line2.CompareTo(line1);
+            int comparison1to3 = line1.CompareTo(line3);
 
-            Console.WriteLine($"Line 1 and Line 2 are equal: {areLinesEqual1}");
-            Console.WriteLine($"Line 1 and Line 3 are equal: {areLinesEqual2}");
+            Console.WriteLine($"Comparison between Line 1 and Line 2: {comparison1to2}");
+            Console.WriteLine($"Comparison between Line 2 and Line 1: {comparison2to1}");
+            Console.WriteLine($"Comparison between Line 1 and Line 3: {comparison1to3}");
         }
     }
 }
